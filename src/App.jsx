0f3894ec903f301973272568d94b12880df35bd6
prathamrathore115 <prathamrathore115@gmail.com>
2025-12-01 12:55:36 +0530

    Create basic structure of app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import './App.css'
+import ChatbotIcon from './assets/chatbot.svg?react';
+import UserIcon from './assets/user.svg?react';
 
 export function ChatInput() {
   return (
@@ -10,4 +12,39 @@ export function ChatInput() {
   );
 }
 
-// export default App
+export function ChatMessage({ message, sender }) {
+  return (
+    <div>
+      {sender === "bot" && <ChatbotIcon width={24} height={24} />}
+      {message}
+      {sender === "user" && <UserIcon width={24} height={24} />}
+    </div>
+  )
+
+}
+
+function App() {
+  return (
+    <>
+      <ChatInput />
+      <ChatMessage
+        message="hello chat bot"
+        sender="user"
+      />
+      <ChatMessage
+        message="hello how can i help you?"
+        sender="bot"
+      />
+      <ChatMessage
+        message="what is date today"
+        sender="user"
+      />
+      <ChatMessage
+        message="its 12/1/2025"
+        sender="bot"
+      />
+    </>
+  );
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {ChatInput} from './App.jsx'
+import App from './App.jsx'
 
-const chatinpt = (
-  <>
-  <ChatInput />
-  </>
-);
+
 createRoot(document.getElementById('container')).render(
   <StrictMode>
-    {chatinpt}
+    <App />
   </StrictMode>,
 )
